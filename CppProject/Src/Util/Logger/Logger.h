2aaad5adb11c1be6033bf55b110cdd16f4cbd8e9
keqing996 <epigrf@outlook.com>
2023-11-26 09:57:26 +0800

    Fix logger bug when multi template para accepted, by using fold expression

--- CppProject/Src/Util/Logger/Logger.h
+++ CppProject/Src/Util/Logger/Logger.h
@@ -35,7 +35,7 @@ namespace Util
         template <class... Types>
         static void LogInfo(const std::format_string<Types...> Fmt, Types&&... Args)
         {
-            LogInfo(std::format(Fmt, std::forward<Types&&...>(Args...)));
+            LogInfo(std::format(Fmt, std::forward<Types>(Args)...));
         }
 
         static void LogWarn(const std::string& message);
@@ -44,7 +44,7 @@ namespace Util
         template <class... Types>
         static void LogWarn(const std::format_string<Types...> Fmt, Types&&... Args)
         {
-            LogWarn(std::format(Fmt, std::forward<Types&&...>(Args...)));
+            LogWarn(std::format(Fmt, std::forward<Types>(Args)...));
         }
 
         static void LogError(const std::string& message);
@@ -53,7 +53,7 @@ namespace Util
         template <class... Types>
         static void LogError(const std::format_string<Types...> Fmt, Types&&... Args)
         {
-            LogError(std::format(Fmt, std::forward<Types&&...>(Args...)));
+            LogError(std::format(Fmt, std::forward<Types>(Args)...));
         }
 
     private:
