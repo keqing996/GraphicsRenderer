910d5fe4d91a4f414e6a4bee8863da897ed78c72
keqing996 <epigrf@outlook.com>
2023-12-04 11:19:50 +0800

    Enable depth test

--- CppProject/Src/RendererHardwareInterface/OpenGL/RenderCommand/RenderCommandOpenGL.cpp
+++ CppProject/Src/RendererHardwareInterface/OpenGL/RenderCommand/RenderCommandOpenGL.cpp
@@ -94,6 +94,9 @@ namespace Renderer
         ::glEnable(GL_BLEND);
         ::glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+        // enable depth test
+        ::glEnable(GL_DEPTH_TEST);
+
         return true;
     }
 
