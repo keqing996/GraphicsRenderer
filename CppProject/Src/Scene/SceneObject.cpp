b63eb1d430e4640bf13887cf41a2c6fa32c14af1
keqing996 <epigrf@outlook.com>
2023-12-05 21:59:12 +0800

    Fix input bug

--- CppProject/Src/Scene/SceneObject.cpp
+++ CppProject/Src/Scene/SceneObject.cpp
@@ -18,14 +18,32 @@ const Eigen::Vector3f& SceneObject::GetScale() const
 void SceneObject::SetPosition(const Eigen::Vector3f& position)
 {
     _position = position;
+
+    for (auto& [key, pComp]: _componentMap)
+    {
+        if (pComp != nullptr)
+            pComp->OnPositionSet();
+    }
 }
 
 void SceneObject::SetRotation(const Eigen::Quaternionf& rotation)
 {
     _rotation = rotation;
+
+    for (auto& [key, pComp]: _componentMap)
+    {
+        if (pComp != nullptr)
+            pComp->OnRotationSet();
+    }
 }
 
 void SceneObject::SetScale(const Eigen::Vector3f& scale)
 {
     _scale = scale;
+
+    for (auto& [key, pComp]: _componentMap)
+    {
+        if (pComp != nullptr)
+            pComp->OnScaleSet();
+    }
 }
