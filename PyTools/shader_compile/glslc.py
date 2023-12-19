fdb8b9c814f3353db4fdc22f87dcac54e28e7336
keqing996 <epigrf@outlook.com>
2023-12-19 13:48:43 +0800

    Shader spirv gen

--- PyTools/shader_compile/glslc.py
+++ PyTools/shader_compile/glslc.py
@@ -5,7 +5,9 @@ from . import tool_path
 
 def compile_glsl(glsl_path: str) -> None:
     print('glslc compile {}'.format(glsl_path))
-    spv_path: str = glsl_path + '.spv'
+    spv_path: str = glsl_path.replace('/Shader/', '/ShaderBin/SpirV/') + '.spv'
+
+    os.makedirs(os.path.dirname(spv_path), exist_ok=True)
 
     cmd = '{} {} -o {} --target-env=opengl'.format(tool_path.get_glslc_path(), os.path.abspath(glsl_path), os.path.abspath(spv_path))
 
--- PyTools/shader_gen.py
+++ PyTools/shader_gen.py
@@ -1,9 +1,13 @@
 import os.path
 from shader_compile import glslc
+from shader_compile import tool_path
 
 
 def main() -> None:
     src_dir: str = '../CppProject/Assets/Shader/'
+
+    tool_path.clear_dir('../CppProject/Assets/ShaderBin/')
+
     for root, dir_list, file_list in os.walk(src_dir):
         for file_name in file_list:
             file_full_path: str = root + '/' + file_name
